d7af609c1dad4a69778d797b0dc573981574b7db
Chae Yungi <l8673901@gmail.com>
2014-01-03 14:00:49 +0900

    간단 Gallery 애플리케이션입니다.
    사진5장만 있습니다.

--- Gallery04/ViewController.m
+++ Gallery04/ViewController.m
@@ -9,15 +9,44 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *imgv2;
 
 @end
 
 @implementation ViewController
+{
+	UIImage *img[5];
+	int c;
+	UIImageView *imgv1;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+- (void)viewDidAppear:(BOOL)animated {
+	[super viewDidAppear:animated];
+	c = 0;
+	
+	NSString *path = [[NSBundle mainBundle] bundlePath];
+	NSString *fileName = [NSString stringWithFormat:@"%@/002.jpg",path];
+	img[0] = [UIImage imageWithContentsOfFile:fileName];
+	fileName = [NSString stringWithFormat:@"%@/ball1.png",path];
+	img[1] = [UIImage imageWithContentsOfFile:fileName];
+	fileName = [NSString stringWithFormat:@"%@/ball2.png",path];
+	img[2] = [UIImage imageWithContentsOfFile:fileName];
+	fileName = [NSString stringWithFormat:@"%@/ball3.png",path];
+	img[3] = [UIImage imageWithContentsOfFile:fileName];
+	fileName = [NSString stringWithFormat:@"%@/sky.jpg",path];
+	img[4] = [UIImage imageWithContentsOfFile:fileName];
+	
+	_imgv2 = [[UIImageView alloc] initWithImage:img[0]];
+	
+	
+	
+	
+	[self.view insertSubview:_imgv2 atIndex:0];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +54,35 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)leftButton:(id)sender {
+	if(c!=0) {
+		c--;
+		_imgv2.image = img[c];
+	}
+}
+- (IBAction)rightButton:(id)sender {
+	if(c!=4) {
+		c++;
+		_imgv2.image = img[c];
+	}
+}
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
